9568527c094610cd424000fbd4fecca6cb85e71d
yan786362806 <786362806@qq.com>
2016-07-01 17:54:33 +0800

    20141105044yanwei.6
    本节课要求用sql
    lite做一个小通讯录，腰先调试移动设备数据库，数据库要有姓名，电话，地址，邮箱等，没有老师指导，一头雾水不只带该如何弄起，先做了个iphone
    上通讯录简单的界面，接下来继续做

---
+++ 20141105044严伟6Tests/_0141105044__6Tests.swift
@@ -0,0 +1,36 @@
+//
+//  _0141105044__6Tests.swift
+//  20141105044严伟6Tests
+//
+//  Created by 20141105044y on 16/7/1.
+//  Copyright © 2016年 20141105044y. All rights reserved.
+//
+
+import XCTest
+@testable import _0141105044__6
+
+class _0141105044__6Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
